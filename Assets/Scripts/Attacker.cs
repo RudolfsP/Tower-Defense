b782f4241fd3b70fb2eb6684805022d1ea4ba38d
RudolfsP <52312006+RudolfsP@users.noreply.github.com>
2020-10-05 12:00:03 +0300

    Initial commit

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+    [Range(0f, 5f)]
+    float currentSpeed = 1f;
+
+    void Update() {
+        //everything in the update method has to be multiplied by Time.deltaTime
+        //to make things framerate independant
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float speed) {
+        currentSpeed = speed;
+    }
+
+}
---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+
+    bool spawn = true;
+    [SerializeField] float minSpawnInterval = 1f;
+    [SerializeField] float maxSpawnInterval = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    IEnumerator Start() {
+        while (spawn) {
+            yield return new WaitForSeconds(Random.Range(minSpawnInterval, maxSpawnInterval));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker() {
+        Instantiate(
+            attackerPrefab,
+            transform.position,
+            Quaternion.identity
+            );
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
