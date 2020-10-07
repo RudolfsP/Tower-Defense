da8eae79cf790990054dca1a937b2f9eeed90426
RudolfsP <52312006+RudolfsP@users.noreply.github.com>
2020-10-07 12:54:35 +0300

    06.10 implements

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    [SerializeField] float health = 100f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] [Range(0, 1)] float deathSoundVolume = 0.4f;
+    [SerializeField] GameObject deathVFX;
+
+    public void DealDamage(float damage) {
+        health -= damage;
+
+        if (health <= 0) {
+            Die();
+        }
+    }
+
+    public void Die() {
+        TriggerDeathVFX();
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
+        Destroy(gameObject);
+
+
+    }
+
+    private void TriggerDeathVFX() {
+        if(!deathVFX) { return; }
+        GameObject particleEffect = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        Destroy(particleEffect, 1f);
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 50f;
+
+    void Update() {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        var attacker = collider.GetComponent<Attacker>();
+        var health = attacker.GetComponent<Health>();
+
+        if (attacker && health) {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
